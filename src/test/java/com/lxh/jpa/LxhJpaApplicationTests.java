6966f02b44ba862166c08bf91f5e7e3d470e11bf
lxh <xcdsy@aliyun.com>
2021-01-02 20:49:10 +0800

    Initial commit

---
+++ src/test/java/com/lxh/jpa/LxhJpaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lxh.jpa;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LxhJpaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
